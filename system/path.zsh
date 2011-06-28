8721827dfcfb8311b574fd66a64d57c7a70fdf37
Stephen Tudor <smt@sunblu.sh>
2011-06-27 20:22:38 -0400

    Update PATH to include cabal/haskell platform

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$HOME/.cabal/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 

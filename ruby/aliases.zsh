1d0ed6484b8eb27f3574bd2f1e8b12aba8161d98
Stephen Tudor <smt@sunblu.sh>
2011-08-03 00:22:09 -0400

    Ruby/RVM update

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,3 +3,4 @@ alias r='rvm use ree'
 alias f='RAILS_ENV=fi'
 
 alias guard='nocorrect guard'
+alias rake='noglob rake'
--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,3 +1,3 @@
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
-rvm use ree
+rvm use 1.9.2

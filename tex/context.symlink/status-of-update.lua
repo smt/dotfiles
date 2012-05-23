d928062d2969117cbdb73214dd6081586f62dd86
Stephen Tudor <smt@sunblu.sh>
2012-05-23 18:36:08 -0400

    Add ConTeXt config

---
+++ tex/context.symlink/status-of-update.lua
@@ -0,0 +1,43 @@
+-- generator : luat-sta.lua
+-- state tag : whatever
+
+return {
+ ["context"]={
+  ["version"]="current",
+ },
+ ["engines"]={
+  ["luatex"]=true,
+  ["pdftex"]=true,
+  ["xetex"]=true,
+ },
+ ["formats"]={
+  ["cont-en"]=true,
+  ["cont-nl"]=true,
+  ["metafun"]=true,
+ },
+ ["info"]={
+  ["comment"]="this file contains the settings of the last 'mtxrun --script update' run",
+  ["count"]=1,
+  ["date"]="2012-05-23 21:42:44",
+  ["runtime"]=72.877,
+  ["version"]=0.1,
+ },
+ ["paths"]={
+  ["root"]="/Users/smt/.context/tex",
+ },
+ ["platforms"]={
+  ["osx-64"]=true,
+ },
+ ["repositories"]={
+  ["current"]=true,
+ },
+ ["rsync"]={
+  ["flags"]={
+   ["delete"]="--delete",
+   ["normal"]="-rpztlv",
+  },
+  ["module"]="minimals",
+  ["program"]="rsync",
+  ["server"]="contextgarden.net::",
+ },
+}
---
+++ tex/tex.zsh
@@ -0,0 +1,3 @@
+# To install ConTeXt, run `./first-setup.sh --context=current`
+
+source $DOTS/tex/context.symlink/tex/setuptex

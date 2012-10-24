6ed54f57545526bb314bfd496c4c0b1ff8814371
Stephen Tudor <smtudor@gmail.com>
2012-10-24 14:40:13 -0400

    Shellconfigs

--- tex/context.symlink/status-of-update.lua
+++ tex/context.symlink/status-of-update.lua
@@ -3,7 +3,7 @@
 
 return {
  ["context"]={
-  ["version"]="current",
+  ["version"]="beta",
  },
  ["engines"]={
   ["luatex"]=true,
@@ -17,13 +17,13 @@ return {
  },
  ["info"]={
   ["comment"]="this file contains the settings of the last 'mtxrun --script update' run",
-  ["count"]=3,
-  ["date"]="2012-05-25 02:41:57",
-  ["runtime"]=43.093,
+  ["count"]=4,
+  ["date"]="2012-10-24 16:59:00",
+  ["runtime"]=64.642,
   ["version"]=0.1,
  },
  ["paths"]={
-  ["root"]="/Users/tudor/.dotfiles/tex/context.symlink/tex",
+  ["root"]="/Users/studor/.dotfiles/tex/context.symlink/tex",
  },
  ["platforms"]={
   ["osx-64"]=true,

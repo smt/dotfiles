b44898e9ff909126e4cc376b54dacb9d5039c3f4
Stephen Tudor <smtudor@gmail.com>
2012-10-26 09:05:25 -0400

    Git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,7 @@
 alias g="git"
 
 # The rest of my fun git aliases
-alias gar="git-addremove"
+alias gaa="git add -A"
 alias gl='git pull --prune'
 alias gam="git commit --amend"
 alias gd="git diff"
@@ -9,11 +9,15 @@ alias gpp="git-up && git push"
 alias gr="git rebase"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gs="git status -sb" # upgrade your git if -sb breaks for you. it's fun.
+alias gf="git flow"
+alias gffs="gf feature start"
+alias gfff="gf feature finish"
+alias gfrs="gf release start"
+alias gfrf="gf release finish"
 
 # Included for legacy purposes
-alias gci="git commit"
-# alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gci="git commit -v"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-alias gsci="git svn dcommit"
-alias gsf="git svn fetch"
-alias gsup="git svn rebase"
+# alias gsci="git svn dcommit"
+# alias gsf="git svn fetch"
+# alias gsup="git svn rebase"

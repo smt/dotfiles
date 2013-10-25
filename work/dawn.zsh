1f10bc4da88557632c85e3908016d22926ab5e54
Stephen Tudor <stephen_tudor@epam.com>
2013-10-25 14:41:15 -0400

    Work-specific settings

--- work/dawn.zsh
+++ work/dawn.zsh
@@ -1,5 +1,7 @@
-export STB_CONF='/Users/studor/Dropbox/Workbench/empathylab/dawn/.user.json'
-export STB_KEY='/Users/studor/Dropbox/Workbench/empathylab/dawn/stb-scripts/keys/stb_rsa'
+export DAWN_PATH="$HOME/Dropbox/Workbench/empathylab/dawn"
+export STB_IP_VGW="172.24.200.147"
+export STB_CONF="$DAWN_PATH/.user.json"
+export STB_KEY="$DAWN_PATH/stb-scripts/keys/stb_rsa"
 
 function createStbShell() {
     local stbip=`node -e "console.log(JSON.parse(require('fs').readFileSync('$STB_CONF')).stb[0].ip)"`
@@ -32,4 +34,22 @@ alias stbtunnel=createStbTunnel
 alias stbshell=createStbShell
 alias stbtelnet=createStbTelnet
 # alias stbtail=tailStbBrowserLogs
-alias stbtail="multitail -l 'ssh -i $STB_KEY root@172.24.200.146 tail -f /opt/logs/mwb.log' -l 'ssh -i $STB_KEY root@172.24.200.146 tail -f /opt/logs/ocapri_log.txt'"
+alias stbtail="multitail -l 'ssh -i $STB_KEY root@$STB_IP_VGW tail -f /opt/logs/mwb.log' -l 'ssh -i $STB_KEY root@$STB_IP_VGW tail -f /opt/logs/ocapri_log.txt'"
+
+function dawnTailStb() {
+    ssh -i $STB_KEY -t root@$STB_IP_VGW 'touch /opt/logs/mwb.log && tail -f /opt/logs/mwb.log' | \
+        # colout "^.*$" Rainbow normal
+        colout "^\".*Z  DEBUG  .*$" none normal | \
+        colout "^\".*controller\.do.*$" cyan normal | \
+        colout "^\"DAWN.*Z - INFO:.*$" blue normal | \
+        colout "^\"DAWN.*Z - DEBUG:.*$" magenta normal | \
+        colout "^\"DAWN.*Z - WARN:.*$" yellow normal | \
+        colout "^\"DAWN.*Z - ERROR:.*$" red normal | \
+        colout "^\"DAWN.*Z.*$" white normal | \
+        colout "^\".*STOPWATCH.*$" green normal
+}
+alias dawn-tail-stb=dawnTailStb
+
+alias touchdusts="find $DAWN_PATH/src/templates/dusts/ -type f | xargs touch"
+
+alias dawn-ssh="ssh -o StrictHostKeyChecking=no -i $STB_KEY root@$STB_IP_VGW"

8c68f4a51bf818c111597bb115163c399b14cc07
Stephen Tudor <smtudor@gmail.com>
2015-04-13 19:06:17 -0400

    Add muttrc

--- topics/mail/mutt/offlineimap.py
+++ topics/mail/mutt/offlineimap.py
@@ -6,12 +6,11 @@ def get_keychain_pass(account=None, server=None):
         'command': 'find-internet-password',
         'account': account,
         'server': server,
-        'keychain': '/Users/studor/Library/Keychains/login.keychain',
+        'keychain': '/Users/`whoami`/Library/Keychains/login.keychain',
     }
-    command = "sudo -u studor %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    command = "sudo -u `whoami` %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
 
     return re.match(r'password: "(.*)"', outtext).group(1)
-

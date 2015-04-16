32847a951dbf9b9676c7339f949c159cff50f413
Stephen Tudor <smtudor@gmail.com>
2015-04-16 10:37:27 -0400

    Brain fart on the ansible path

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -116,7 +116,7 @@ fi
 
 # Provision the box
 echo "Info   | Configure   | ansible-playbook"
-$ANSIBLE_DIRECTORY/ansible-playbook -e "username=$(whoami)" -v --ask-sudo-pass -i $ANSIBLE_CONFIGURATION_DIRECTORY/inventories/osx $ANSIBLE_CONFIGURATION_DIRECTORY/site.yml --connection=local
+$ANSIBLE_DIRECTORY/bin/ansible-playbook -e "username=$(whoami)" -v --ask-sudo-pass -i $ANSIBLE_CONFIGURATION_DIRECTORY/inventories/osx $ANSIBLE_CONFIGURATION_DIRECTORY/site.yml --connection=local
 
 # Link the casks.
 echo "Info   | Configure   | link-casks"

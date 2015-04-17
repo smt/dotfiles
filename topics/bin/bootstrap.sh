96891b67be6ff42f0bdc2bf6ec4915b78465c2e0
Stephen Tudor <smtudor@gmail.com>
2015-04-17 16:51:58 -0400

    Keep-alive sudo

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -16,6 +16,12 @@ else
     return 1
 fi
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `bootstrap.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Download and install Command Line Tools
 xcode-select -p > /dev/null 2>&1
 if [ $? -ne 0 ]; then
